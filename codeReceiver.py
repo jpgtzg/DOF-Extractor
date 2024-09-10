704ee9ecb24f520fed4f3b000cffbf3288c20ec2
jpgtzg <58833781+jpgtzg@users.noreply.github.com>
2024-09-10 08:41:16 -0600

    Moved code to independent functions

--- codeReceiver.py
+++ codeReceiver.py
@@ -1,39 +1,35 @@
+"""
+    Writtn by Juan Pablo Gutiérrez
+    09 09 2024
+"""
+
 import requests
-from datetime import date
+from datetime import datetime
 
-date = date.today().strftime("%d-%m-%Y")
+def getAllCodes() -> list:
+    date = datetime.today().strftime("%d-%m-%Y")
+    
+    dateEndpoint = f'https://sidofqa.segob.gob.mx/dof/sidof/diarios/porFecha/{
+        date}'
+    x = requests.get(dateEndpoint).json()
 
-dateEndpoint = f'https://sidofqa.segob.gob.mx/dof/sidof/diarios/porFecha/{
-    date}'
-x = requests.get(dateEndpoint).json()
+    matutina = x["Matutina"]
+    vespertina = x["Vespertina"]
+    extraordinaria = x["Extraordinaria"]
 
-matutina = x["Matutina"]
-vespertina = x["Vespertina"]
-extraordinaria = x["Extraordinaria"]
+    codeList = list()
 
-codeList = list()
+    if matutina:
+        for i in matutina:
+            codeList.append(i["codDiario"])
 
-if matutina:
-    for i in matutina:
-        codeList.append(i["codDiario"])
-
-if vespertina:
-    for i in vespertina:
-        codeList.append(i["codDiario"])
+    if vespertina:
+        for i in vespertina:
+            codeList.append(i["codDiario"])
 
 
-if extraordinaria:
-    for i in extraordinaria:
-        codeList.append(i["codDiario"])
+    if extraordinaria:
+        for i in extraordinaria:
+            codeList.append(i["codDiario"])
 
-print(codeList)
-
-for i in codeList:
-    diaryEndpoint = f'https://sidofqa.segob.gob.mx/dof/sidof/documentos/pdf/{
-        i}'
-
-    result = requests.get(diaryEndpoint)
-
-    with open('result.pdf', 'wb') as f:
-        for chunk in result.iter_content(2000):
-            f.write(chunk)
+    return codeList
--- downloader.py
+++ downloader.py
@@ -1,1 +1,18 @@
+"""
+    Writtn by Juan Pablo Gutiérrez
+    09 09 2024
+"""
+
 import requests
+
+def download(code : int):
+    diaryEndpoint = f'https://sidofqa.segob.gob.mx/dof/sidof/documentos/pdf/{code}'
+    result = requests.get(diaryEndpoint)
+    with open(f'{code}.pdf', 'wb') as f:
+        for chunk in result.iter_content(2000):
+            f.write(chunk)
+
+
+def downloadAll(codeList : list):
+    for i in codeList:
+        download(i)
