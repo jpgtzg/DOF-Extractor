bb96a2ecb31a5e267f54cde64df9cbc3e9cc46e1
jpgtzg <58833781+jpgtzg@users.noreply.github.com>
2024-09-11 08:32:50 -0600

    Changed return to tuple

--- algorithms/diary_filtering.py
+++ algorithms/diary_filtering.py
@@ -8,8 +8,8 @@ from collections import defaultdict
 
 key = ["Fugas", "Registradores", "Telemetría", "Dataloggers", "Eficiencia", "Correlador", "Geófono", "Multicorrelador", "Logger", "Pérdidas", "Recuperación"]
 
-def checkForKeys(code: int) -> bool:
-    lower_keys = [k.lower() for k in key] # Convert all keys to lowercase
+def checkForKeys(code: int, key: list) -> tuple:
+    lower_keys = [k.lower() for k in key]  # Convert all keys to lowercase
     foundKeys = defaultdict(int)  # Initialize a defaultdict to count occurrences
 
     reader = PdfReader(f'{code}.pdf')
@@ -25,4 +25,4 @@ def checkForKeys(code: int) -> bool:
     for k, v in foundKeys.items():
         print(f'{k}: {v}')
 
-    return len(foundKeys) > 0
+    return (len(foundKeys) > 0, dict(foundKeys))
